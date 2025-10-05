63675a7c69335fc0cca1a095691e92477180a533
GonzaloAxelCode <gonzaloaxelcode@gmail.com>
2025-10-04 21:04:41 -0500

    add filters

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	production: true,
-	URL_BASE: 'https://backend-accesories.onrender.com',
-	URL_BASE_FRONT: 'https://inventario-web-axel-drab.vercel.app',
+	URL_BASE: 'https://apiexample.gonzaloaxelcode.workers.dev',
+	URL_BASE_FRONT: 'admin-geoapp-pro.vercel.app',
 	API_VERSION: '1.0',
 };
